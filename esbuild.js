718d2fbad6ae7daaa3a2dc66450837d8a0079339
lv-saharan <saharan@foxmail.com>
2022-09-22 17:44:45 +0800

    bug fix,remove css function cache

--- esbuild.js
+++ esbuild.js
@@ -2,6 +2,7 @@
 let pkg = require('./package.json')
 let [mode] = process.argv.splice(2);
 let outfile = `../es-lib/omii/${pkg.version}/omii.js`;
+//let outfile = `../es-lib/omii/latest/omii.js`;
 let options = {
     entryPoints: ['src/omii.js'],
     outfile,
