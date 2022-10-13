604eccf6f076a18d6365c1a1d5fb531953925124
lv-saharan <saharan@foxmail.com>
2022-10-13 15:34:28 +0800

    add local-dev-server

--- esbuild.js
+++ esbuild.js
@@ -1,16 +1,35 @@
 
-let pkg = require('./package.json')
-let [mode] = process.argv.splice(2);
+import pkg from './package.json'  assert { type: "json" }
+import esbuild from 'esbuild'
+import { dev } from "local-dev-server"
+const [mode] = process.argv.splice(2);
 //let outfile = `../es-lib/omii/${pkg.version}/omii.js`;
-let outfile = `./dist/omii.js`;
+const outfile = `./dist/omii.js`;
 //let outfile = `../es-lib/omii/latest/omii.js`;
-let options = {
+const options = {
     entryPoints: ['src/omii.js'],
     outfile,
     format: "esm",
     bundle: true,
     sourcemap: true,
-    minify: mode == "minify"
+    minify: true
 }
-const esbuild = require('esbuild')
-esbuild.buildSync(options)
+
+
+if (mode == "dev") {
+    const { reload } = dev({ ...pkg.localDev.server, openBrowser: false })
+    esbuild.build({
+        ...options, watch: {
+            onRebuild(error, result) {
+                if (error) console.error('watch build failed:', error)
+                else {
+                    console.log('watch build succeeded:', result)
+                    reload("ui rebuild ok")
+                }
+
+            },
+        }
+    })
+} else {
+    esbuild.buildSync(options)
+}
